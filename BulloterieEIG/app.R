c80871ba9e3562b00e6242c7be1c52111e0ecf17
raphaelle.roffo <raphaelle.roffo@gmail.com>
2021-02-22 22:30:56 +0100

    added Design System CSS

--- BulloterieEIG/app.R
+++ BulloterieEIG/app.R
@@ -5,16 +5,58 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+
+
+
+
+
+
+setwd('/Users/raph/Documents/Etalab/Bulloterie/bulloterie')
+# Libraries
+library(tidyverse)
+library(hrbrthemes)
+#devtools::install_github("jeromefroe/circlepackeR")
+library(circlepackeR)
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+# Load dataset from github
+data <- read.csv2("Bulloterie_Clean.csv", header=T, sep=";")
+#data[ which(data$value==-1),"value"] <- 1
+
+data <- as.data.frame(gsub("[[:punct:]]", "", as.matrix(data)))
+
+# Change the format. This use the data.tree library. This library needs a column that looks like root/group/subgroup/..., so I build it
+library(data.tree)
+
+data$pathString <- paste("EIG", data$Metier, data$Theme, data$Sujet, sep = "/")
+
+data <- data %>% group_by(Sujet) %>% mutate(count = n())
+
+bulle <- as.Node(data)
+
+# You can custom the minimum and maximum value of the color range.
+circlepackeR(bulle, size = "Prend", color_min = "hsl(152,80%,80%)", color_max = "hsl(228,30%,40%)")
+
+
+
+
 
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    theme = "all.min.css",
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    # Logo and application title
+    tags$img(height = 100, src = "https://raw.githubusercontent.com/entrepreneur-interet-general/site-eig/811e28c13afa2d6d981d6ec1ae2cc1a7876cf6d1/img/eig_.svg"),
+    titlePanel("La Bulloterie EIG"),
+    tags$br(),
+    tags$p("L'ensemble des centres d'intérêts de la promo EIG 4 sont rassemblés ici. Pour trouver qui veut apprendre ou
+           transmettre les mêmes sujets que toi, utilise la barre de recherche !"),
+    tags$img({circlepackeR(bulle, size = "Prend",
+                           color_min = "hsl(152,80%,80%)", color_max = "hsl(228,30%,40%)")}),
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
@@ -47,3 +89,4 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
